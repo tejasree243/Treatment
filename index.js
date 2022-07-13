d059f0d5b88071b35175ac21c8c3372b36f982de
KaushanthiDevi <109232020+KaushanthiDevi@users.noreply.github.com>
2022-07-14 00:28:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
 //const uri = 'mongodb://localhost:27017';
 // Connection URL
-const uri = 'mongodb+srv://kaushanthi:chinnu123@clusterkaush.jshvwc0.mongodb.net/universe?retryWrites=true&w=majority';
+const uri = process.env.goodgirl;
 //const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 //const client = new MongoClient(url);
